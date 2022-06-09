acfcf5567d1a6e8fa2ffdd4e1abdbbee116e91ea
Tendayi T Makwara <tendayimakwara@gmail.com>
2022-06-09 23:28:37 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+import os
+import telebot
+import imdb
+
+my_secret = "5582564501:AAGHLIE4vzPumS8v1vDMuRBKfDToBQ4SmJY"
+bot = telebot.TeleBot(my_secret)
+
+@bot.message_handler(func=lambda m: True)
+def searchlibrary(message):
+  ia = imdb.IMDb()
+  name = message.text
+  search = ia.search_movie(name)
+  id = search[0].movieID
+  series = ia.get_movie(id)
+  rating = str(series.data['rating'])
+  plot = str(series.data['plot'])
+  response = ((search[0]['title']) + ": " + rating + '\n' + plot)
+  bot.reply_to(message,response)
+  
+
+bot.polling()
